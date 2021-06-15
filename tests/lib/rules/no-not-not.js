20927f7b99a44675539365ae91ab3b3e2adad4c3
Sofen Anonta <anonta@outlook.com>
2021-06-16 00:10:10 +0600

    Added test cases for no double not rule

--- tests/lib/rules/no-not-not.js
+++ tests/lib/rules/no-not-not.js
@@ -9,20 +9,60 @@ var rule = require("../../../lib/rules/no-not-not"),
 RuleTester = require("eslint").RuleTester;
 
 var ruleTester = new RuleTester();
+
+const expectedErrorMessage = "Double not operators not allowed";
+const expectedNodeType = "UnaryExpression";
+const defaultError = {
+    message: expectedErrorMessage,
+    type: expectedNodeType,
+};
+
 ruleTester.run("no-not-not", rule, {
 
     valid: [
-
-        // give me some code that won't trigger a warning
+        "var x = Boolean(x % 2);",
+        "var x = !p;",
+        "var x = !a || !b;",
+        "var x = -a",
+        "var x = --a",
+        "var x = +a",
+        "var x = ++a",
+        "var x = ~a",
+        "var x = ~~a",
+        "var x = '!!a';",
+        'var x = "!!a";',
     ],
 
     invalid: [
         {
+            code: "var xIsOdd = !!p;",
+            output: "var xIsOdd = Boolean(p);",
+            errors: [defaultError],
+        },
+        {
             code: "var xIsOdd = !!(x % 2);",
-            errors: [{
-                message: "Double not operators not allowed",
-                type: "UnaryExpression"
-            }]
-        }
+            output: "var xIsOdd = Boolean(x % 2);",
+            errors: [defaultError],
+        },
+        {
+            code: "var x = !(!p);",
+            output: "var x = Boolean(p);",
+            errors: [defaultError]
+        },
+        {
+            code: "var x = (!!a || b);",
+            output: "var x = (Boolean(a) || b);",
+            errors: [defaultError]
+        },
+        {
+            code: "var x = !!a || !!b;",
+            output: "var x = Boolean(a) || Boolean(b);",
+            errors: [defaultError, defaultError]
+        },
+        {
+            code: "var x = (!!a || !!b && !!c);",
+            output: "var x = (Boolean(a) || Boolean(b) && Boolean(c));",
+            errors: [defaultError, defaultError, defaultError]
+        },
     ]
 });
